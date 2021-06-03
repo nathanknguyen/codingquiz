6e0ebfe72256907cb5d81c6970f1551c191dd4ce
Nathan Nguyen <nathanknguyen96@gmail.com>
2021-06-02 17:33:07 -0700

    added variables to start and functions

---
+++ script.js
@@ -0,0 +1,16 @@
+var questionsCorrect = 0;
+var totalQuestions = 
+// need start button
+// need next question button
+// need submit button when on the last question
+// new screen to display final results
+
+function takeQuiz() {
+    
+    // return "Your score is + score"
+}
+
+function calculateScore() {
+    var score = questionsCorrect / totalQuestions;
+    return score;
+}
